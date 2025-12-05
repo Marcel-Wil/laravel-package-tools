63748fdecbedebd08527ee452897152f582e925a
Marcel-Wil <wilczynskimarceli@gmail.com>
2025-12-05 13:08:05 +0100

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,7 +7,7 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    public static ?TestResponse $latestResponse = null;
+    public static $latestResponse;
 
     protected function setUp(): void
     {
