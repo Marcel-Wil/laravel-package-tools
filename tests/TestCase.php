4735e97e322e518d3e864fa58ea566efab6902fd
Marcel-Wil <wilczynskimarceli@gmail.com>
2025-12-05 10:36:29 +0100

    add latestResponse property

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,13 @@
 
 namespace Spatie\LaravelPackageTools\Tests;
 
+use Illuminate\Testing\TestResponse;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
+    public static ?TestResponse $latestResponse = null;
+
     protected function setUp(): void
     {
         parent::setUp();
